97db72b59ce49e06b2e30c88d443d17eb96fc3a6
Helena Batista <111362141+helenabatiista@users.noreply.github.com>
2025-06-01 22:26:41 -0300

    Add files via upload

---
+++ testes-automatizados-e2e-front-serve-rest_cypress-main/cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 1280,
+  viewportHeight: 720,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      const environment = config.env.environment || 'prod'
+      config.env = require(`./cypress/support/environments/${environment}.json`)
+      config.baseUrl = config.env.baseUrl
+      const baseUri = config.env.baseUri
+      
+      return config
+    }
+  }
+})
---
+++ testes-automatizados-e2e-front-serve-rest_cypress-main/cypress/support/commands.js
@@ -0,0 +1,97 @@
+/// <reference types="cypress" />
+import selectors from './selectors'
+
+Cypress.Commands.add('realizarLogin', (email = '', senha = '') => {
+  cy.get(selectors.LOGIN.BOTAO_ENTRAR).as('botaoEntrar')
+
+  if (email !== '') {
+    cy.get(selectors.LOGIN.CAMPO_EMAIL)
+      .should('be.visible')
+      .click()
+      .type(email)
+  }
+
+  if (senha !== '') {
+    cy.get(selectors.LOGIN.CAMPO_SENHA)
+      .should('be.visible')
+      .click()
+      .type(senha, { log: false })
+  }
+
+  cy.get('@botaoEntrar')
+    .should('be.visible')
+    .click()
+})
+
+Cypress.Commands.add('cadastrarUsuario', (nome = '', email = '', senha = '') => {
+  cy.get(selectors.CADASTRO_USUARIO.OPCAO_ADMIN).as('opcaoAdmin')
+
+  if (nome !== '') {
+    cy.get(selectors.CADASTRO_USUARIO.CAMPO_NOME)
+      .should('be.visible')
+      .click()
+      .type(nome)
+  }
+
+  if (email !== '') {
+    cy.get(selectors.LOGIN.CAMPO_EMAIL)
+      .should('be.visible')
+      .click()
+      .type(email)
+  }
+
+  if (senha !== '') {
+    cy.get(selectors.LOGIN.CAMPO_SENHA)
+      .should('be.visible')
+      .click()
+      .type(senha, { log: false })
+  }
+
+  cy.get('@opcaoAdmin')
+    .should('be.visible')
+    .click()
+
+  cy.get(selectors.CADASTRO_USUARIO.BOTAO_CADASTRAR)
+    .should('be.visible')
+    .click()
+})
+
+// App Actions
+Cypress.Commands.add('obterPorEmailEexcluirUsuarioPorIdPelaAPI', (email) => {
+  cy.request({
+    url: Cypress.env('baseUri') + `/usuarios?email=${email}`,
+    method: 'GET',
+  }).then(response => {
+    let retornoGet = response.body
+
+    if ((retornoGet.quantidade) == 1) {
+      let idUsuarioRetornado = retornoGet.usuarios[0]._id
+
+      cy.request({
+        url: Cypress.env('baseUri') + `/usuarios/${idUsuarioRetornado}`,
+        method: 'DELETE',
+      }).then((response) => {
+        expect(response.status).to.eql(200)
+      })
+    }
+  })
+})
+
+Cypress.Commands.add('cadastrarUsuarioPelaAPI', (nome, email, senha, admin) => {
+  cy.request({
+    url: Cypress.env('baseUri') + '/usuarios',
+    method: 'POST',
+    headers: {
+      'accept': 'application/json', 
+      'content-type': 'application/json'
+    },
+    body: {
+      "nome": nome,
+      "email": email,
+      "password": senha,
+      "administrador": admin
+    }
+  }).then((response) => {
+    expect(response.status).to.eql(201)
+  })
+})
